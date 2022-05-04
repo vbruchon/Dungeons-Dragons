dfd9872871ed16f9769044367a258375797245ff
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-05-04 08:46:04 +0200

    Create Project

---
+++ src/Personnage/Guerrier.java
@@ -0,0 +1,10 @@
+package Personnage;
+
+public class Guerrier {
+    public static void main(String[] args) {
+        String nom;
+        int niveau de_vie = (5-10);
+        int force d’attaque = (5-10);
+        String arme;
+        String bouclier;    }
+}
---
+++ src/Personnage/Magicien.java
@@ -0,0 +1,4 @@
+package Personnage;
+
+public class Magicien {
+}
